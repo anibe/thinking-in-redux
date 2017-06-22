62a06add8df7a1d4794bb9037efd93f4ad996033
Anibe Agamah <anibe.agamah@asos.com>
2017-06-22 16:23:26 +0100

    Moved menu toggle to redux store

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 export const RECEIVE_BEERS = 'RECEIVE_BEERS'
 export const ADD_BEER_TO_CART = 'ADD_BEER_TO_CART'
+export const TOGGLE_SIDE_MENU = 'TOGGLE_SIDE_MENU'
 // export const REMOVE_BEER_FROM_CART = 'REMOVE_BEER_FROM_CART'
 
 export const receiveBeers = beers => ({
@@ -10,6 +11,10 @@ export const receiveBeers = beers => ({
 export const addBeerToCart = beer => ({
   type: ADD_BEER_TO_CART,
   beer
+})
+
+export const toggleSideMenu = () => ({
+  type: TOGGLE_SIDE_MENU
 })
 
 // export const removeBeerFromCart = beer => ({
--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,14 @@
 import React from "react"
 import SideMenu from 'react-burger-menu'
-
+import { connect } from 'react-redux'
 import BeerList from "./beers/List"
 import Basket from "./cart/Basket"
 import Checkout from "./cart/Checkout"
 import Header from "./Header"
 import theme from "./theme"
+import * as actions from '../actions'
 
 class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      isSideMenuOpen: false
-    }
-  }
-
-  toggleSideMenu = () => {
-    this.setState({ isSideMenuOpen: !this.state.isSideMenuOpen })
-  }
 
   render() {
     return (
@@ -26,7 +17,7 @@ class App extends React.Component {
           noOverlay
           customCrossIcon={false}
           customBurgerIcon={ false }
-          isOpen={this.state.isSideMenuOpen}
+          isOpen={this.props.menu}
           pageWrapId={'page-wrap'}
           styles={theme.sideMenu}
         >
@@ -34,7 +25,7 @@ class App extends React.Component {
           <Basket />
         </SideMenu.slide>
         <div className="container">
-          <a className="fa-stack fa-lg" style={theme.checkoutButton} onClick={this.toggleSideMenu} >
+          <a className="fa-stack fa-lg" style={theme.checkoutButton} onClick={() => this.props.dispatch(actions.toggleSideMenu())} >
             <i className="fa fa-circle fa-stack-2x"></i>
             <i className="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
           </a>
@@ -46,4 +37,15 @@ class App extends React.Component {
   }
 }
 
-export default App
+const mapStateToProps = state => ({
+  menu: state.menu
+})
+
+const mapDispatchToProps = dispatch => ({
+  dispatch
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
